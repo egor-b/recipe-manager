bbdff300318bd90c2c4ac401749b87f9de2b3b94
Egor Bryzgalov <bryzgalov.e@yahoo.com>
2021-02-15 15:20:43 -0500

    optimized imports

--- src/main/java/com/foodcrunch/foodster/recipemanager/auth/model/UserEntity.java
+++ src/main/java/com/foodcrunch/foodster/recipemanager/auth/model/UserEntity.java
@@ -2,7 +2,6 @@ package com.foodcrunch.foodster.recipemanager.auth.model;
 
 import lombok.Data;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
